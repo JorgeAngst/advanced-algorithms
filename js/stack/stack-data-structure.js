b65c0b2dd395590cefa351d80ad72b6e194a9af5
JorgeAngst <jorge.rodriguez.web@gmail.com>
2020-05-12 19:12:13 +0200

    iteration 1 done

---
+++ js/stack/stack-data-structure.js
@@ -0,0 +1,36 @@
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+  }
+
+  canPush() {
+   return this.stackControl.length < this.MAX_SIZE ? true : false;
+  }
+
+  display() {
+    
+  }
+
+  isEmpty() {
+    return this.stackControl.length <= 0;
+  }
+
+  push(item) {
+    
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {  
+      return 'Stack Overflow';
+    }
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return 'Stack Underflow'
+    } else {
+      return this.stackControl.pop()
+    }
+  }
+}
